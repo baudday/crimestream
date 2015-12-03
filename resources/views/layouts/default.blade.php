0ab573058efad4226fb17c5691e39903bec7ba6c
Willem Ellis <willem.ellis@gmail.com>
2015-12-03 17:41:43 -0600

    add analytics script

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,6 +12,16 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/leaflet.js"></script>
     <script src='js/Leaflet.MakiMarkers.js'></script>
     <script src='//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js'></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-71019703-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
     @yield('head-stuff')
   </head>
   <body>
