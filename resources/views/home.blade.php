a5471bdb6441687bbd87631ca2afd2d9f61a92b6
Willem Ellis <willem.ellis@gmail.com>
2015-12-06 13:14:14 -0600

    adjust minzoom

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,7 +27,7 @@
     window.map = L.map('map').setView([36.1314, -95.9372], 12);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-      minZoom: 12,
+      minZoom: 10,
       id: 'mapbox.light',
       accessToken: 'pk.eyJ1IjoiYmF1ZGRheSIsImEiOiJBWkFQV2NJIn0.k1sZSEElIyTFmvVLemkZnA'
     }).addTo(window.map);
