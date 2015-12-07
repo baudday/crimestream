37ad1f8d8b0a21ac22286051b1ffc10faeff2414
Willem Ellis <willem.ellis@gmail.com>
2015-12-07 15:39:53 -0600

    run tweet job every 6 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('scrape:all')->cron('*/3 * * * * *');
-        $schedule->command('notify:twitter')->cron('*/9 * * * * *');
+        $schedule->command('notify:twitter')->cron('*/6 * * * * *');
     }
 }
