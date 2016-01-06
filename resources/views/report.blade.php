b455d6923f04859c8514ce1ce5bfe5259b543963
Willem Ellis <willem.ellis@gmail.com>
2016-01-06 14:28:21 -0600

    scale the dots

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -38,25 +38,27 @@
       var cfg = {
         // radius should be small ONLY if scaleRadius is true (or small radius is intended)
         // if scaleRadius is false it will be the constant radius used in pixels
-        "radius": 10,
+        "radius": .005,
         "maxOpacity": .5,
         // scales the radius based on map zoom
-        "scaleRadius": false,
+        "scaleRadius": true,
         // if set to false the heatmap uses the global maximum for colorization
         // if activated: uses the data maximum within the current map boundaries
         //   (there will always be a red spot with useLocalExtremas true)
-        "useLocalExtrema": false,
+        "useLocalExtrema": true,
         // which field name in your data represents the latitude - default "lat"
         latField: 'lat',
         // which field name in your data represents the longitude - default "lng"
-        lngField: 'lng'
+        lngField: 'lng',
+        // which field name in your data represents the data value - default "value"
+        valueField: 'val'
       };
 
 
       var heatmapLayer = new HeatmapOverlay(cfg);
       var heat = {
         data: data.map(function(el) {
-          return {lat: el.lat, lng: el.lng, value: 0};
+          return {lat: el.lat, lng: el.lng, val: 1};
         })
       };
       window.map.addLayer(heatmapLayer);
