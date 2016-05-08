5a2c7c2ed032137d5aa425804c7c84bb3fa3071e
Willem Ellis <willem.ellis@gmail.com>
2016-05-08 18:26:43 -0500

    refactor heatmap query logic

--- app/Crime.php
+++ app/Crime.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Lib\Location;
+use Carbon\Carbon;
 
 class Crime extends Model
 {
@@ -16,6 +17,49 @@ class Crime extends Model
       $options['lat'] = $geo->lat;
       $options['lng'] = $geo->lng;
       return parent::create($options);
+    }
+
+    public function scopeLikeSlug($query, $slug)
+    {
+      $filters = [
+        'accidents' => [
+          ['description','like','%inj%'],
+          ['description','like','%col%']
+        ],
+        'assaults' => [
+          ['description','like','%aslt%'],
+          ['description','like','%assault%']
+        ],
+        'auto-thefts' =>
+          ['description','like','%theft%'],
+        'burglaries' => [
+          ['description','like','%vehicle%'],
+          ['description','like','%burglary%']
+        ],
+        'burglaries-from-vehicles' => [
+          ['description','like','%abandoned%'],
+          ['description','like','%vehicle%']
+        ],
+        'disturbances' =>
+          ['description','like','%disturbance%'],
+        'hit-runs' =>
+          ['description','like','%hit&amp;run%'],
+        'missing-persons' =>
+          ['description','like','%missing%'],
+        'shootings' => [
+          ['description','like','%shot%'],
+          ['description','like','%shooting%']
+        ]
+      ];
+
+      // Only get the last 3 months of data. Should be plenty
+      return $query->select('*')
+        ->where('created_at', '>=', Carbon::now()->subMonths(3)->toDateTimeString())
+        ->where(function ($q) use ($filters, $slug) {
+          foreach($filters[$slug] as $condition) {
+              call_user_func_array([$q, 'orWhere'], $condition);
+          }
+        });
     }
 
     public function scopeNear($query, $lat, $lng, $distance = 0.25)
--- app/Http/Controllers/Api/Filter.php
+++ app/Http/Controllers/Api/Filter.php
@@ -20,69 +20,7 @@ class Filter extends Controller
     public function index(Request $request)
     {
         if (!$slug = $request->input('slug')) return;
-        switch ($slug) {
-            case 'accidents':
-                $filter = [
-                    ['description','like','%inj%'],
-                    ['description','like','%col%'],
-                ];
-                break;
-            case 'assaults':
-                $filter = [
-                    ['description','like','%aslt%'],
-                    ['description','like','%assault%'],
-                ];
-                break;
-            case 'auto-thefts':
-                $filter = [
-                    ['description','like','%theft%'],
-                ];
-                break;
-            case 'burglaries':
-                $filter = [
-                    ['description','like','%vehicle%'],
-                    ['description','like','%burglary%'],
-                ];
-                break;
-            case 'burglaries-from-vehicles':
-                $filter = [
-                    ['description','like','%abandoned%'],
-                    ['description','like','%vehicle%'],
-                ];
-                break;
-            case 'disturbances':
-                $filter = [
-                    ['description','like','%disturbance%']
-                ];
-                break;
-            case 'hit-runs':
-                $filter = [
-                    ['description','like','%hit&amp;run%']
-                ];
-                break;
-            case 'missing-persons':
-                $filter = [
-                    ['description','like','%missing%']
-                ];
-                break;
-            case 'shootings':
-                $filter = [
-                    ['description','like','%shot%'],
-                    ['description','like','%shooting%'],
-                ];
-                break;
-        }
-
-        // Only get the last 3 months of data. Should be plenty
-        $query = \App\Crime::select('*')
-          ->where('created_at', '>=', Carbon::now()->subMonths(3)->toDateTimeString())
-          ->where(function ($q) use ($filter) {
-            foreach($filter as $condition) {
-                call_user_func_array([$q, 'orWhere'], $condition);
-            }
-          });
-
-        $crimes = $query->get();
+        $crimes = \App\Crime::likeSlug($slug)->get();
         return response()->json($crimes);
     }
 
