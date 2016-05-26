6845f9a5790419d86d752594d988fa7901f08a4c
Willem Ellis <willem.ellis@gmail.com>
2016-05-26 14:37:58 -0500

    let's use a non-copyrighted image for now

--- resources/views/splash.blade.php
+++ resources/views/splash.blade.php
@@ -48,7 +48,7 @@
     var img = $('.bg-img');
     var downloadingImg = new Image();
 
-    downloadingImg.src = '/img/splash_header.jpg';
+    downloadingImg.src = '/img/bg_alt.jpg';
 
     downloadingImg.onload = function() {
       img.css({
