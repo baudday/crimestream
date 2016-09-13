87243bb0e05e6c24828d3c64a01cbb720f0025e5
Willem Ellis <willem.ellis@gmail.com>
2016-09-13 10:49:45 -0500

    admin email. duh

--- app/Http/Middleware/SubscriptionMiddleware.php
+++ app/Http/Middleware/SubscriptionMiddleware.php
@@ -15,7 +15,7 @@ class SubscriptionMiddleware
      */
     public function handle($request, Closure $next)
     {
-      if ($request->user() && ! $request->user()->subscribed('main')) {
+      if ($request->user() && ! $request->user()->subscribed('main') && $request->user()->email !== getenv('ADMIN_EMAIL')) {
         $request->session()->flash('error', 'You must be subscribed to use this feature. It\'s only $5 a month!');
         return redirect('account');
       }
